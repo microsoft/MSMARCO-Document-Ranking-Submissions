30942140d7a9d8c2b4475b4796c6b814b1aabd82
lintool <jimmylin@uwaterloo.ca>
2020-12-14 15:54:51 -0500

    Added command-line arguments.

--- eval/ms_marco_doc_eval.py
+++ eval/ms_marco_doc_eval.py
@@ -1,20 +1,17 @@
 """
-This module computes evaluation metrics for MSMARCO dataset on the ranking task. Intenral hard coded eval files version. DO NOT PUBLISH!
-Command line:
-python msmarco_eval_ranking.py <path_to_candidate_file>
+Official evaluation script for the MS MARCO Document Ranking task.
 
-Creation Date : 06/12/2018
-Last Modified : 08/06/2020
-Authors : Daniel Campos <dacamp@microsoft.com>, Rutger van Haasteren <ruvanh@microsoft.com>
+Authors: Daniel Campos, Rutger van Haasteren, Jimmy Lin
 """
-import sys
+
+import argparse
 import re
 import os
-import statistics
 
 from collections import Counter
 
 MaxMRRRank = 100
+
 
 def autoopen(filename, mode="rt"):
     """
@@ -50,6 +47,7 @@ def load_reference_from_stream(f):
             raise IOError('\"%s\" is not valid format' % l)
     return qids_to_relevant_documentids
 
+
 def load_reference(path_to_reference):
     """Load Reference reference relevant document
     Args:path_to_reference (str): path to a file to load.
@@ -59,10 +57,12 @@ def load_reference(path_to_reference):
         qids_to_relevant_documentids = load_reference_from_stream(f)
     return qids_to_relevant_documentids
 
+
 def validate_candidate_has_enough_ranking(qid_to_ranked_candidate_documents):
     for qid in qid_to_ranked_candidate_documents:
         if len(qid_to_ranked_candidate_documents[qid]) > MaxMRRRank:
             print('Too many documents ranked. Please Provide top 100 documents for qid:{}'.format(qid))
+
 
 def load_candidate_from_stream(f):
     """Load candidate data from a stream.
@@ -87,6 +87,8 @@ def load_candidate_from_stream(f):
     validate_candidate_has_enough_ranking(qid_to_ranked_candidate_documents)
     print('Quantity of Documents ranked for each query is as expected. Evaluating')
     return {qid: sorted(qid_to_ranked_candidate_documents[qid], key=lambda x:(x[1], x[0]), reverse=False) for qid in qid_to_ranked_candidate_documents}         
+
+
 def load_candidate(path_to_candidate):
     """Load candidate data from a file.
     Args:path_to_candidate (str): path to file to load.
@@ -96,6 +98,7 @@ def load_candidate(path_to_candidate):
     with autoopen(path_to_candidate,'r') as f:
         qid_to_ranked_candidate_documents = load_candidate_from_stream(f)
     return qid_to_ranked_candidate_documents
+
 
 def quality_checks_qids(qids_to_relevant_documentids, qids_to_ranked_candidate_documents):
     """Perform quality checks on the dictionaries
@@ -125,6 +128,7 @@ def quality_checks_qids(qids_to_relevant_documentids, qids_to_ranked_candidate_d
             allowed = False
 
     return allowed, message
+
 
 def compute_metrics(qids_to_relevant_documentids, qids_to_ranked_candidate_documents, exclude_qids):
     """Compute MRR metric
@@ -158,7 +162,8 @@ def compute_metrics(qids_to_relevant_documentids, qids_to_ranked_candidate_docum
     all_scores['MRR @100'] = MRR
     all_scores['QueriesRanked'] = len(set(qids_to_ranked_candidate_documents)-exclude_qids)
     return all_scores
-                
+
+
 def compute_metrics_from_files(path_to_reference, path_to_candidate, exclude_qids, perform_checks=True):
     """Compute MRR metric
     Args:    
@@ -175,7 +180,6 @@ def compute_metrics_from_files(path_to_reference, path_to_candidate, exclude_qid
     Returns:
         dict: dictionary of metrics {'MRR': <MRR Score>}
     """
-    
     qids_to_relevant_documentids = load_reference(path_to_reference)
     qids_to_ranked_candidate_documents = load_candidate(path_to_candidate)
     if perform_checks:
@@ -183,6 +187,8 @@ def compute_metrics_from_files(path_to_reference, path_to_candidate, exclude_qid
         if message != '': print(message)
 
     return compute_metrics(qids_to_relevant_documentids, qids_to_ranked_candidate_documents, exclude_qids)
+
+
 def load_exclude(path_to_exclude_folder):
     """Load QIDS for queries to exclude
     Args: 
@@ -202,26 +208,28 @@ def load_exclude(path_to_exclude_folder):
     print("{} excluded qids loaded".format(len(qids)))
     return qids
 
-def main():
-    """Command line:
-    python document_ranking.py <path_to_candidate_file> <path_to_reference_file> <queries_to_exclude>
-    """
-    if len(sys.argv) == 1:
-        #print("Usage:  document_ranking.py <path_to_candidate_file> <path_to_reference_file> <queries_to_exclude>") for public version
-        print("Usage:  document_ranking.py <path_to_candidate_file> ")
-    else:
-        print(sys.argv)
-        exclude_qids = set()
-        if len(sys.argv) == 4:
-            exclude_qids = load_exclude(sys.argv[3]) #Public implementation
-        #exclude_qids = load_exclude('exclude/')
-        path_to_candidate = sys.argv[1]
-        path_to_reference = sys.argv[2]
-        metrics = compute_metrics_from_files(path_to_reference, path_to_candidate, exclude_qids)
-        print('#####################')
-        for metric in sorted(metrics):
-            print('{}: {}'.format(metric, metrics[metric]))
-        print('#####################')
+
+def main(args):
+    # Load optional excludes.
+    exclude_qids = set()
+    if args.exclude:
+        exclude_qids = load_exclude(args.exclude)
+
+    # Load run and judgments.
+    path_to_candidate = args.run
+    path_to_reference = args.judgments
+
+    metrics = compute_metrics_from_files(path_to_reference, path_to_candidate, exclude_qids)
+    print('#####################')
+    for metric in sorted(metrics):
+        print('{}: {}'.format(metric, metrics[metric]))
+    print('#####################')
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Official evaluation script for the MS MARCO Document Ranking task.')
+    parser.add_argument('--run', type=str, metavar='file', required=True, help='Run file.')
+    parser.add_argument('--judgments', type=str, metavar='file', required=True, help='Judgments.')
+    parser.add_argument('--exclude', type=str, metavar='file', required=False, help='Exclude directory.')
+
+    main(parser.parse_args())
