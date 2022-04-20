5239c20c8a2dbb9a199c58a58caa6937dc8d31e2
lintool <jimmylin@uwaterloo.ca>
2022-04-20 07:56:49 -0400

    Updated leaderboard plots.

--- analysis/plot_leaderboard_over_time.py
+++ analysis/plot_leaderboard_over_time.py
@@ -18,14 +18,20 @@ df = pd.read_csv('../leaderboard/leaderboard.csv', parse_dates=['date'])
 # Plot all the runs
 ax = df.plot(x='date',y='MRR@100 (Eval)',marker='o',linestyle='none',label='Submission')
 
+# Overlay all the runs that have 'BERT' in their names, in orange
+bert = df[df['description'].str.contains('BERT', case=False)]
+bert.plot(ax=ax, x='date',y='MRR@100 (Eval)',marker='o',color = 'orange',linestyle='none',label='"BERT" technique')
+
 # Overlay all SOTA runs, in red.
 sota = df[df['Unnamed: 2'] == '🏆']
 sota.plot(ax=ax, x='date',y='MRR@100 (Eval)',marker='o',color = 'red',linestyle='none',label='SOTA')
 
 # Guide to formatting date ticks
 # https://matplotlib.org/3.1.1/gallery/text_labels_and_annotations/date.html
-ax.xaxis.set_major_locator(mdates.MonthLocator())
-ax.set_xlim([datetime.date(2020, 8, 1), datetime.date(2021, 5, 1)])
+
+# Do this if we insist on plotting everyone month; otherwise let the package decide
+# ax.xaxis.set_major_locator(mdates.MonthLocator())
+ax.set_xlim([datetime.date(2020, 8, 1), datetime.date(2022, 5, 1)])
 
 plt.title('MS MARCO Document Leaderboard')
 plt.xlabel('Date')
